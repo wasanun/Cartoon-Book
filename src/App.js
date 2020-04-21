a50d6c0332ee14dece76f425755b1afbef010142
wasanun <wasanun_04@hotmail.com>
2020-04-21 19:18:40 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+ 
+  return (
+    
+    <div className="App">
+     
+      <header className="App-header">
+         <h1>Cartoon Book </h1>
+        
+      </header>
+      <body className="App-body">
+
+      </body>
+
+      <footer className="App-link">
+    <p> หมายเหตุ กรุณาติดต่อ</p>
+
+  </footer>   
+    </div>
+  );
+  
+}
+
+export default App;
