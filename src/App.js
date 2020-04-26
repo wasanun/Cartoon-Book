87d176d546b0ac0edd16c9458d08c1a1a292a24b
wasanun <wasanun_04@hotmail.com>
2020-04-26 16:38:42 +0700

    Todo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './App.css';
 import im from './im/e.jpg'
+import Item1 from './im/1.png'
+import im1 from './im/3.jpg'
+import im2 from './im/5.jpg'
+import im4 from './im/6.jpg'
+import im5 from './im/7.jpg'
+import Todo from './components/todo';
+
 
 function App() {
  
@@ -13,13 +20,28 @@ function App() {
         
       </header>
       <body className="App-body">
-      <img src={im} alt='Avatar photo' width="500px" /> <br/>
+       
+      <img src={im} alt='photo' width="100px" /> 
+      <img src={im1} alt='photo' width="100px" /> 
+      <img src={im2} alt='photo' width="100px" /> 
+      <img src={im4} alt='photo' width="100px" /> <br/>
+      <img src={im5} alt='photo' width="100px" /> 
+      <img src={Item1} alt='photo' width="100px" /> 
+
+
+      <Todo head="Todo" />
+
+
+
       </body>
 
+
       <footer className="App-link">
-    <p> หมายเหตุ กรุณาติดต่อ</p>
+    <p> 5935512043</p>
 
   </footer>   
+
+
     </div>
   );
   
---
+++ src/components/todo.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';//เอาโค๊ดมากจาก Githubของอาจารย์วโรดม
+
+class Todo extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {list: ['หนังสือโดเรม่อน', 'หนังสือชินจัง','นิยาย',]};
+    }
+    add = (e) => {
+        let item = this.refs.todo.value;
+        console.log('todo',item);
+        this.setState( {list: [...this.state.list, item] });
+
+    }
+    renderList = () => {
+        return (
+            this.state.list.map((item,index) => {
+                return ( <li key={index}> {index+1} : {item} </li>)
+            })
+        )
+    }
+    render() {
+        return (
+            <div className="App">
+                    <div className="App-title App-header">
+                        <h2> {this.props.head} </h2>
+                    </div>
+
+                <div className="App-content">
+
+                    <div className="row">
+                        <div className="col75">
+                            <input type="text" ref="todo"  />
+                            <button onClick={this.add}>Add</button>
+                         
+                        </div>
+
+                    </div>
+
+                    <div>
+                        {this.renderList()}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Todo;
